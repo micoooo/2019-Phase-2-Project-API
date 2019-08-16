6f22bab325f194a4e77ca816aba75bf460e7c7a4
Michael Chung <michaelchungng@live.com>
2019-08-17 04:24:12 +1200

    unitesting files

---
+++ TodoAPIUnitTests/TodoCountrollerUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TODOlistAPI.Controllers;
+using TODOlistAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace TodoAPIUnitTests {
+    public class TodoCountrollerUnitTests {
+        [TestClass]
+        public TodoCountrollerUnitTests
+            {
+            public static readonly DbContextOptions<TODOlistContext> options = new DbContextOptionsBuilder<TODOlistContext>()
+            .UseInMemoryDatabase(databaseName: "testDatabase")
+            .Options;
+    }
+    }
+    }
