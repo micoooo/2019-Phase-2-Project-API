0421e8ee2bf1df5ee031e9dabccf9381194f45ed
Michael Chung <michaelchungng@live.com>
2019-08-16 14:06:13 +1200

    initial commit

---
+++ TODOlistAPI/Model/TODOlistContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TODOlistAPI.Model
+{
+    public partial class TODOlistContext : DbContext
+    {
+        public TODOlistContext()
+        {
+        }
+
+        public TODOlistContext(DbContextOptions<TODOlistContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Todo> Todo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:todoolist.database.windows.net,1433;Initial Catalog=TODOlist;Persist Security Info=False;User ID=micoo;Password=Lollies1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>(entity =>
+            {
+                entity.Property(e => e.TaskDescription).IsUnicode(false);
+
+                entity.Property(e => e.TaskTitle).IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ TODOlistAPI/Model/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TODOlistAPI.Model
+{
+    [Table("TODO")]
+    public partial class Todo
+    {
+        [Key]
+        [Column("TaskID")]
+        public int TaskId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string TaskTitle { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string TaskDescription { get; set; }
+    }
+}
